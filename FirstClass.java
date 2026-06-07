2d64e0fd671753c3fa975fc2c5585f63b6a27a33
Piyush Yadav <piyushyadavv16@gmail.com>
2026-06-07 19:54:20 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,23 @@
+class FirstClass{
+public static void main( String args[]) {
+     System.out.println("Hello World");
+}
+
+}
+
+
+class FirstClass{
+public static void main( String args[]) {
+     System.out.println("my name is piyush yadav");
+}
+
+}  
+
+
+class FirstClass{
+
+   public static void main( String args[]){
+     System.out.print("*\n**\n***\n****\n*****");
+   }
+}
+
---
+++ Variables.java
@@ -0,0 +1,15 @@
+class Variables{
+
+    public static void main(String args[]){
+         // String name = "Piyush Yadav";
+          int a = 10;
+          int b = 5;
+         
+          int sum = a+b;
+         
+          System.out.println(sum);
+         
+          int diff = a-b;
+          System.out.print(diff);
+    }
+}
