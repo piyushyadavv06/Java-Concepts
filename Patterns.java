94f0db54cee9a19b9b6fd988f963b045d249c8fc
Piyush Yadav <piyushyadavv16@gmail.com>
2026-06-18 17:38:46 +0530

    Add files via upload

--- Patterns.java
+++ Patterns.java
@@ -1,136 +1,136 @@
-// class Patterns{
+class Patterns{
 
-//     public static void main(String args[]){
+    public static void main(String args[]){
 
-//         for(int i = 1 ; i<=4 ; i++){
-//             for(int j = 1 ; j<=5 ; j++){
-//                 System.out.print("* ");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
+        for(int i = 1 ; i<=4 ; i++){
+            for(int j = 1 ; j<=5 ; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
 
 
 
-// class Patterns{
+class Patterns{
 
-//     public static void main(String args[]){
+    public static void main(String args[]){
 
-// int n = 4;
-// int m = 5;
-//         for(int i = 1 ; i<=n ; i++){
-//             for(int j = 1 ; j<=m ; j++){
-//                 if(i==1 || j == 1 || j == m || i == n) 
-//                 System.out.print("*");
+int n = 4;
+int m = 5;
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=m ; j++){
+                if(i==1 || j == 1 || j == m || i == n) 
+                System.out.print("*");
             
-//             else
-//             System.out.print(" ");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
+            else
+            System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
 
 
 
-// class Patterns{
+class Patterns{
 
-//     public static void main(String args[]){
-//         int n = 4;
-//         int m = 4;
+    public static void main(String args[]){
+        int n = 4;
+        int m = 4;
 
-//         for(int i = 1 ; i<=n ; i++){
-//             for(int j = 1 ; j<=i ; j++){
-//                 System.out.print("* ");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
-
-
-
-// class Patterns{
-
-//     public static void main(String args[]){
-//         int n = 4;
-//         int m = 4;
-
-//         for(int i = n ; i>=1 ; i--){
-//             for(int j = 1 ; j<=i ; j++){
-//                 System.out.print("* ");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
 
 
 
+class Patterns{
 
-// public class Patterns {
-//    public static void main(String args[]) {
-//        int n = 4;
+    public static void main(String args[]){
+        int n = 4;
+        int m = 4;
+
+        for(int i = n ; i>=1 ; i--){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+
+
+
+public class Patterns {
+   public static void main(String args[]) {
+       int n = 4;
       
-//        for(int i=1; i<=n; i++) {
-//            for(int j=1; j<=n-i; j++) {
-//                System.out.print(" ");
-//            }
+       for(int i=1; i<=n; i++) {
+           for(int j=1; j<=n-i; j++) {
+               System.out.print(" ");
+           }
 
 
-//            for(int j=1; j<=i; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//    }
-// }
+           for(int j=1; j<=i; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+   }
+}
 
 
 
-// public class Patterns {
-//    public static void main(String args[]) {
-//        int n = 5;
+public class Patterns {
+   public static void main(String args[]) {
+       int n = 5;
       
-//        for(int i=1; i<=n; i++) {
-//            for(int j=1; j<=i; j++) {
-//                System.out.print(j);
-//            }
-//            System.out.println();
-//        }
-//    }
-// }
+       for(int i=1; i<=n; i++) {
+           for(int j=1; j<=i; j++) {
+               System.out.print(j);
+           }
+           System.out.println();
+       }
+   }
+}
 
 
 
-// public class Patterns {
-//    public static void main(String args[]) {
-//        int n = 5;
+public class Patterns {
+   public static void main(String args[]) {
+       int n = 5;
       
-//        for(int i=n; i>=1; i--) {
-//            for(int j=1; j<=i; j++) {
-//                System.out.print(j);
-//            }
-//            System.out.println();
-//        }
-//    }
-// }
+       for(int i=n; i>=1; i--) {
+           for(int j=1; j<=i; j++) {
+               System.out.print(j);
+           }
+           System.out.println();
+       }
+   }
+}
 
 
 
-// public class Patterns {
-//    public static void main(String args[]) {
-//        int n = 5;
-//        for(int i=1; i<=n; i++) {
-//            for(int j=1; j<=i; j++) {
-//               if((i+j) % 2 == 0){
-//                 System.out.print(1 + " ");
-//               }
-//               else
-//               System.out.print(0 + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
-// }
+public class Patterns {
+   public static void main(String args[]) {
+       int n = 5;
+       for(int i=1; i<=n; i++) {
+           for(int j=1; j<=i; j++) {
+              if((i+j) % 2 == 0){
+                System.out.print(1 + " ");
+              }
+              else
+              System.out.print(0 + " ");
+           }
+           System.out.println();
+       }
+   }
+}
